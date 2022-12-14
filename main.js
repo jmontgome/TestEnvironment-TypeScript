abb34eb4fb5bf4f066073c7c6758d58aab889e95
Jonathon Montgomery <87336298+jmontgome@users.noreply.github.com>
2022-12-14 12:44:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var message = "Hello World, string setter!";
+console.log(message);
+var message2 = "Hello World, loose setter";
+console.log(message2);
+var list = ["Hello", "My", "Name", "is", "Com"];
+for (var i = 0; i < 30; i++) {
+    console.log(i);
+}
+list.forEach(function (element) {
+    console.log(element);
+});
---
+++ main.ts
@@ -0,0 +1,15 @@
+let message: string = "Hello World, string setter!";
+console.log(message);
+
+var message2 = "Hello World, loose setter";
+console.log(message2);
+
+let list: Array<string> = ["Hello", "My", "Name", "is", "Com"]
+
+for (var i = 0; i < 30; i++) {
+    console.log(i);
+}
+
+list.forEach(element => {
+    console.log(element);
+});
